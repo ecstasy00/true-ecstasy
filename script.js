6722c7e025f4d11a4e49e62521161a5adf522f44
ecstasy00 <myecstasy0@gmail.com>
2025-02-15 22:13:48 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = async function() {
+    try {
+        const response = await fetch('/api/twitter-auth');
+        const data = await response.json();
+        document.getElementById('oauth-link').href = data.oauth_url;
+    } catch (error) {
+        console.error("Error fetching OAuth link:", error);
+    }
+};
